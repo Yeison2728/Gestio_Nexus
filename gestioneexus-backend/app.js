f79ce81dc91d4c5aad830a960452233c4a7d0236
Yeison Molina <yeisonmolinabeltran@gmail.com>
2025-08-20 15:21:52 -0500

    Fix: Corrige error en app.js y dependencias

--- gestioneexus-backend/app.js
+++ gestioneexus-backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = 'morgan';
+const morgan = require('morgan'); // <--- CAMBIA ESTA LÍNEA
 const path = require('path');
 require('dotenv').config();
 
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev')); // <-- AÑADE ESTA LÍNEA PARA USARLO
+
 
 // Rutas
 app.use('/api/auth', require('./routes/auth.routes'));
